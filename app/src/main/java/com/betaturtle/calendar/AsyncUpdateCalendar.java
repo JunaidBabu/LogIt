54a13f54d5a83f055a641bb02203710317a88c62
Junaid Babu <junaidvayoli@gmail.com>
2014-09-07 23:09:30 +0530

    More spices. Onam Special :P

---
+++ app/src/main/java/com/betaturtle/calendar/AsyncUpdateCalendar.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2012 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.betaturtle.calendar;
+
+import android.util.Log;
+import android.view.View;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Calendar;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Asynchronously updates a calendar with a progress dialog.
+ * 
+ * @author Yaniv Inbar
+ */
+class AsyncUpdateCalendar extends CalendarAsyncTask {
+
+  private final String calendarId;
+  private final Calendar entry;
+
+  AsyncUpdateCalendar(CalendarSampleActivity calendarSample, String calendarId, Calendar entry) {
+    super(calendarSample);
+    this.calendarId = calendarId;
+    this.entry = entry;
+  }
+
+  @Override
+  protected void doInBackground() throws IOException {
+    try {
+      Calendar updatedCalendar =
+          client.calendars().patch(calendarId, entry).setFields(CalendarInfo.FIELDS).execute();
+      Log.w("Calendar ID", calendarId);
+        
+        EventCreate(calendarId, entry.getSummary());
+    } catch (GoogleJsonResponseException e) {
+      // 404 Not Found would happen if user tries to delete an already deleted calendar
+      if (e.getStatusCode() != 404) {
+        throw e;
+      }
+      model.remove(calendarId);
+    }
+  }
+
+    public void EventCreate(String calendarId, String entry) throws IOException {
+        Event event = newEvent(entry);
+        Event result = client.events().insert(calendarId, event).execute();
+        Log.w("Result", result.toString());
+    }
+
+    private static Event newEvent(String summary) {
+        Event event = new Event();
+        event.setSummary(summary);
+        Date startDate = new Date();
+        Date endDate = new Date(startDate.getTime() + 5*60*1000);
+        DateTime start = new DateTime(startDate, TimeZone.getTimeZone("UTC"));
+        event.setStart(new EventDateTime().setDateTime(start));
+        DateTime end = new DateTime(endDate, TimeZone.getTimeZone("UTC"));
+        event.setEnd(new EventDateTime().setDateTime(end));
+        return event;
+    }
+}
--- app/src/main/java/in/junaidbabu/logit/MainActivity.java
+++ app/src/main/java/in/junaidbabu/logit/MainActivity.java
@@ -1,22 +1,20 @@
 package in.junaidbabu.logit;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Toast;
 
+import com.betaturtle.calendar.R;
 import com.cengalabs.flatui.FlatUI;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
     static MySQLiteHelper db;
     com.cengalabs.flatui.views.FlatEditText logbox;
     com.cengalabs.flatui.views.FlatButton button;
@@ -26,7 +24,7 @@ public class MainActivity extends ActionBarActivity {
         db = new MySQLiteHelper(this);
         setContentView(R.layout.activity_main);
         FlatUI.setDefaultTheme(FlatUI.GRASS);
-        FlatUI.setActionBarTheme(this, FlatUI.GRASS, false, false);
+        //FlatUI.setActionBarTheme(this, FlatUI.GRASS, false, false);
         logbox = (com.cengalabs.flatui.views.FlatEditText)findViewById(R.id.logbox);
         button = (com.cengalabs.flatui.views.FlatButton)findViewById(R.id.button);
         logbox.addTextChangedListener(new TextWatcher() {
@@ -59,26 +57,6 @@ public class MainActivity extends ActionBarActivity {
         Toast.makeText(this, Type+" "+Time+" "+Text, Toast.LENGTH_SHORT).show();
         logbox.setText("");
         button.setText("Logged!");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
